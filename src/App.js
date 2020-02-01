0cd436cf313b67ca13c53ca72acb07b4d1bb12c9
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-01 02:30:25 +0100

    Created router with links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
 
-    </div>
+    // </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        
+        <Switch>
+          <Route exact path="/">{/* <Home /> */}</Route>
+          <Route path="/about">{/* <About /> */}</Route>
+          <Route path="/users">{/* <Users /> */}</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
