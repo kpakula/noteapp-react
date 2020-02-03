43245c9f309b7fd1b9adcbba61b2669572bd67bd
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-03 18:32:49 +0100

    Modified All available users view

--- src/components/allUser/AllUser.js
+++ src/components/allUser/AllUser.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import axios from "axios";
+import usersURL from "./../../Api/api.js"
 
 export default class AllUser extends Component {
     state = {
         allUsers: [],
     };
 
+    componentDidMount() {
+        axios.get(usersURL).then(res => {
+            console.log(res.data)
+            this.setState({ allUsers: res.data });
+        })
+    }
 
     render() {
         return (
