fa351ed4924ddad9c5835d942d88906b53d744b9
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-05 16:34:49 +0100

    Added redirect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import AllUser from "./components/allUser/AllUser";
 import NoMatch from "./components/noMatch/NoMatch";
 import specificUser from "./components/specificUser/specificUser";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route exact path="/">{/* <Home /> */}</Route>
-          <Route exact path="/signin">{/* <Home /> */}</Route>
-          <Route exact path="/signup">{/* <Home /> */}</Route>
+          <Route exact path="/"><Redirect to="/signin"/></Route>
+          <Route exact path="/signin"><Login/></Route>
+          <Route exact path="/signup"><Register/></Route>
           <Route exact path="/users"><AllUser/></Route>
           <Route path="/users/:id" component={specificUser}></Route>
           <Route component={NoMatch}></Route>
