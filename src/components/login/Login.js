3682dec14336bc6b362e7ec3f654180075038657
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-06 19:36:29 +0100

    Refactored login and register view

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,15 +1,40 @@
 import React from "react";
 import { Button, Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <Row>
-      <Col>
-        <Button>Hejka</Button>
-      </Col>
+<Row className="justify-content-center align-items-center h-100">
+      <Col xs={10} sm={8} md={5} lg={4} className="leftSide">
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Login</label>
+            <input
+              type="email"
+              class="form-control mb-0"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter password"
+            />
+          </div>
+          <small id="emailHelp" class="form-text text-light">You don't have account? <Link to="/signup">Sign up</Link></small>
 
-      <Col>
-        <Button>Hejka</Button>
+          <button
+            type="submit"
+            class="btn btn-secondary w-50 mt-2"
+          >
+            Login
+          </button>
+        </form>
       </Col>
     </Row>
   );
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,40 +1,10 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import "./Register.css";
+import { Link } from "react-router-dom";
 
 export default function Register() {
   return (
-    <Row className="justify-content-center align-items-center h-100">
-      <Col sm={8} md={5} lg={4} className="leftSide">
-          <form>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Email address</label>
-              <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="Enter email"
-              />
-              <small id="emailHelp" class="form-text text-muted">
-                We'll never share your email with anyone else.
-              </small>
-            </div>
-            <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
-              <input
-                type="password"
-                class="form-control"
-                id="exampleInputPassword1"
-                placeholder="Password"
-              />
-            </div>
-            <button type="submit" class="btn btn-secondary mt-2">
-              Login
-            </button>
-          </form>
-      </Col>
-      {/* <Col className="rightSide">Right</Col> */}
-    </Row>
+    <Link to="/signin" ><b>Sign in</b></Link>
   );
 }
