428b6e30fc4b17edd83fdf7627fd109238860c74
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-06 19:36:34 +0100

    Login view alpha version

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,41 +1,55 @@
 import React from "react";
-import { Button, Col, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export default function Login() {
-  return (
-<Row className="justify-content-center align-items-center h-100">
-      <Col xs={10} sm={8} md={5} lg={4} className="leftSide">
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Login</label>
-            <input
-              type="email"
-              class="form-control mb-0"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-              placeholder="Enter password"
-            />
-          </div>
-          <small id="emailHelp" class="form-text text-light">You don't have account? <Link to="/signup">Sign up</Link></small>
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      login: "",
+      password: ""
+    };
+  }
 
-          <button
-            type="submit"
-            class="btn btn-secondary w-50 mt-2"
-          >
-            Login
-          </button>
-        </form>
-      </Col>
-    </Row>
-  );
+  handleSubmit = (event) => {
+    console.log(`Login: ${this.state.login} \n Password: ${this.state.password}`);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <Row className="justify-content-center align-items-center h-100">
+        <Col xs={10} sm={8} md={5} lg={4} className="leftSide">
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="exampleInputLogin">Login</label>
+              <input
+                type="email"
+                className="form-control mb-0"
+                id="exampleInputLogin"
+                aria-describedby="loginHelp"
+                placeholder="Enter login"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Enter password"
+              />
+            </div>
+            <small id="emailHelp" className="form-text text-light">
+              You don't have account? <Link className="text-dark" to="/signup"><b>Sign up</b></Link>
+            </small>
+
+            <button type="submit" className="btn btn-secondary w-50 mt-2">
+              Login
+            </button>
+          </form>
+        </Col>
+      </Row>
+    );
+  }
 }
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
 import "./Register.css";
 import { Link } from "react-router-dom";
 
