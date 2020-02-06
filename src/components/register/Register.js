19509bfc3eb520cc20d9abd3f9e2459c07d4c9f2
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-06 19:36:18 +0100

    Height on all page

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import "./Register.css";
+
 export default function Register() {
   return (
-    <Row className="h-100">
+    <Row className="justify-content-center h-100">
       <Col className="leftSide">Left</Col>
       <Col className="rightSide">Right</Col>
     </Row>
