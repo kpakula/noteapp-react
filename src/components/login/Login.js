0ce719988336860739aabed5e85a521950815d5f
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-07 19:36:45 +0100

    Added login and password handler

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,17 +4,27 @@ import { Link } from "react-router-dom";
 
 export default class Login extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       login: "",
       password: ""
     };
   }
 
-  handleSubmit = (event) => {
-    console.log(`Login: ${this.state.login} \n Password: ${this.state.password}`);
+  handleSubmit = event => {
+    console.log(
+      `Login: ${this.state.login} \n Password: ${this.state.password}`
+    );
     event.preventDefault();
-  }
+  };
+
+  handleLoginInput = event => {
+    this.setState({ login: event.target.value });
+  };
+
+  handlePassword = event => {
+    this.setState({ password: event.target.value });
+  };
 
   render() {
     return (
@@ -27,8 +37,9 @@ export default class Login extends React.Component {
                 type="email"
                 className="form-control mb-0"
                 id="exampleInputLogin"
-                aria-describedby="loginHelp"
                 placeholder="Enter login"
+                value={this.state.login}
+                onChange={this.handleLoginInput}
               />
             </div>
             <div className="form-group">
@@ -38,10 +49,15 @@ export default class Login extends React.Component {
                 className="form-control"
                 id="exampleInputPassword1"
                 placeholder="Enter password"
+                value={this.state.password}
+                onChange={this.handlePassword}
               />
             </div>
             <small id="emailHelp" className="form-text text-light">
-              You don't have account? <Link className="text-dark" to="/signup"><b>Sign up</b></Link>
+              You don't have account?{" "}
+              <Link className="text-dark" to="/signup">
+                <b>Sign up</b>
+              </Link>
             </small>
 
             <button type="submit" className="btn btn-secondary w-50 mt-2">
