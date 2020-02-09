912cf07021c465340cca1e890a1bb1d9857027d8
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-09 19:37:57 +0100

    New Notes view Created

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function HomeView() {
     return (
-        <div>
-            
+        <div id="notesWrapper">
+            <div className="tc-notes">
+                <div className="tc-note">
+                    <div className="tc-note-header">
+
+                        
+                    </div>
+                    <div className="tc-note-title">
+                        Title goes here
+                    </div>
+
+                    <div className="tc-note-body">
+                        Body goes here
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
