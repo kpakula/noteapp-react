5580a379a3076f7b396fd949ce46c7a11060becc
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-11 21:38:49 +0100

    HomeViewUpdated

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import { Col, Row, Container } from "react-bootstrap";
 import "./HomeView.css";
 import Note from "./notes/Note";
 
-export default function HomeView() {
-  return (
-    <div class="card-deck justify-content-center mt-5">
-      <Col xl={2} lg={3} md={4} sm={6} xs={12}>
+export default class HomeView extends Component {
+  state = {
+    allNotes: []
+  };
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div class="card-deck justify-content-center mt-5">
+        {/* <Col xl={2} lg={3} md={4} sm={6} xs={12}>
         <Note />
       </Col>
       <Col xl={2} lg={3} md={4} sm={6} xs={12}>
@@ -20,7 +27,8 @@ export default function HomeView() {
       </Col>
       <Col xl={2} lg={3} md={4} sm={6} xs={12}>
         <Note />
-      </Col>
-    </div>
-  );
+      </Col> */}
+      </div>
+    );
+  }
 }
